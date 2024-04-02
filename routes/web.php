3d537973a752e4ab1f8a3fc07da345720547ede9
imiantalha <talhaarshad427@gmail.com>
2024-04-02 22:46:57 +0500

    Revert "Lot of modifications"
    This reverts commit 3c388c7e55566be33f5112797b741cc974c242c7.

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,6 @@
 <?php
 
-use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-use App\Models\User;
-// use Illuminate\Support\Facades\DB;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,60 +14,7 @@ use App\Models\User;
 */
 
 Route::get('/', function () {
-    // return view('welcome');
-
-    //find id 1
-    // $user = DB::table('users')->find(1);
-    //first user
-    // $user = DB::table('users')->first();
-
-    // $user = User::find(1);
-    // $user ->update([
-    //     'email' => 'MuhammaD@gmail.com'
-    // ]);
-
-
-    // create new user
-    // $user = DB::insert('insert into users (name, email, password) values (?,?,?)', [
-    //     'Ali',
-    //     'ali1@gmail.com',
-    //     'password1'
-    // ]);
-
-    // $user = DB::table('users')->insert([
-    //     'name' => 'Abdullah',
-    //     'email' => 'abdullah@gmail.com',
-    //     'password' => 'password'
-    // ]);
-
-                                //uses two function in model User.php
-    // $user = User::create([ 
-    //     'name' => 'Talha',
-    //     'email' => 'talha4@gmail.com',
-    //     'password' => 'password'
-    // ]);
-
-
-    // $users = DB::select('select * from users');
-
-
-    // $user = DB::table('users')->where('id', 3)->delete();
-
-    
-    // $users = DB::table('users')->get();
-    $users = User::find(14);
-    dd($users->name);
-
+    return view('welcome');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
-
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
-
-require __DIR__.'/auth.php';
+Route::view('/home', 'home');
